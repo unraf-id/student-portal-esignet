130f5eefd86b7c71e2c442307e22c543859f6d06
Avanish Duggireddy <avanish.duggireddy@infystrat.com>
2024-10-23 19:28:55 +0530

    Update Background.js

--- oidc-ui/src/components/Background.js
+++ oidc-ui/src/components/Background.js
@@ -68,11 +68,11 @@ export default function Background({
         {component}
       </div>
       {/* Enable the signup banner when it is true in the signup.config of oauth-details */}
-      {signupBanner && 
+      {/* {signupBanner && 
       <div className="signup-banner">
         <p className="signup-banner-text">{t("noAccount")}</p>
         <a className="signup-banner-hyperlink" href={signupURL} target="_self" onClick={() => handleSignup()}>{t("signup_for_unified_login")}</a>
-      </div>}
+      </div>} */}
     </div>
   );
 }
